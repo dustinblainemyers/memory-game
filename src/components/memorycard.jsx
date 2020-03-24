c5bfded5afff102f6a33b347f330e3b0c9582f22
Dustin Myers <dustinblaine.myers@gmail.com>
2020-03-24 11:36:39 -0400

    MemoryCard component created

---
+++ src/components/memorycard.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+    class MemoryCard extends Component {
+        render() {
+            return (
+                <h1>Card</h1>
+            );
+        }
+    }
+
+export default MemoryCard;
