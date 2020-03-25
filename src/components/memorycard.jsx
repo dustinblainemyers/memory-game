6a6d76bdf64b50e28dcffbbed6ab8ab75140fef6
Dustin Myers <dustinblaine.myers@gmail.com>
2020-03-25 09:35:43 -0400

    part 2 done through step 5

--- src/components/memorycard.jsx
+++ src/components/memorycard.jsx
@@ -1,19 +1,30 @@
 import React, { Component } from "react";
 import './MemoryCard.css';
 
-    class MemoryCard extends Component {
-        render() {
-            let array = []
-                for (let i = 0;i < 4; i ++) {
-                array.push(<div className="MemoryCard">
-                <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt="digitalcrafts logo"/>
-                </div>)
-                }
-            return (
-                array
-            );
-            
+class MemoryCard extends Component {
+    render() {
+        let array = []
+        for (let i = 0; i < 4; i++) {
+            array.push(
+                <div className="MemoryCard">
+                    <div className="MemoryCardInner">
+                        <div className="MemoryCardBack">
+                            <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt="digitalcrafts logo" />
+                        </div>
+                        
+                        <div className="MemoryCardFront">
+                            ∆
+                        </div>
+                    </div>
+                </div>
+
+            )
         }
+        return (
+            array
+        );
+
     }
+}
 
 export default MemoryCard;
