3df0ed0d06d88c06262553e0f4cb121679bbd95c
Dustin Myers <dustinblaine.myers@gmail.com>
2020-03-26 10:48:13 -0400

    part 3 step 1 done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React, {Component} from "react";
-
-
 import "./App.css";
 import MemoryCard from "./components/memorycard.jsx";
+
+function generateDeck() {
+  const symbols = [`∆`,` ß`, `£`, `§`,`•`, `$`, `+`, `ø`];
+  const deck = [];
+
+  for (let i = 0; i < 16;i ++ ) {
+    deck.push({
+      isFlipped: false,
+      Symbol: symbols[i % 8]
+    });
+  }
+
+  shuffle(deck);
+  return deck;
+}
+
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+  }
+  return a;
+}
 
 class App extends Component {
   constructor() {
     super();
     this.state = { 
       
-      deck: [],
+      deck: generateDeck(),
       pickedCards: []
     };
   }
