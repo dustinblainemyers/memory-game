fb0a581b390ff22fe8cef3adbdd78c8ebf1b73dd
Dustin Myers <dustinblaine.myers@gmail.com>
2020-03-26 10:34:45 -0400

    converting app component from functional to class

--- src/App.js
+++ src/App.js
@@ -1,49 +1,49 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-import MemoryCard from "./components/memorycard.jsx"
+import "./App.css";
+import MemoryCard from "./components/memorycard.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Memory Game</h1>
-        <h2>Match Cards To Win</h2>
-
-        
-      </header>
-      <div className="grid-container">
-        <div className="grid-row">
-          <MemoryCard/>
-          <MemoryCard/>
-          <MemoryCard/>
-          <MemoryCard/>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { isFlipped: false };
+  }
+  App() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <h1>Memory Game</h1>
+          <h2>Match Cards To Win</h2>
+        </header>
+        <div className='grid-container'>
+          <div className='grid-row'>
+            <MemoryCard />
+            <MemoryCard />
+            <MemoryCard />
+            <MemoryCard />
+          </div>
+          <div className='grid-row'>
+            <MemoryCard />
+            <MemoryCard />
+            <MemoryCard />
+            <MemoryCard />
+          </div>
+          <div className='grid-row'>
+            <MemoryCard />
+            <MemoryCard />
+            <MemoryCard />
+            <MemoryCard />
+          </div>
+          <div className='grid-row'>
+            <MemoryCard />
+            <MemoryCard />
+            <MemoryCard />
+            <MemoryCard />
+          </div>
         </div>
-        <div className="grid-row">
-          <MemoryCard/>
-          <MemoryCard/>
-          <MemoryCard/>
-          <MemoryCard/>
-        </div>
-        <div className="grid-row">
-          <MemoryCard/>
-          <MemoryCard/>
-          <MemoryCard/>
-          <MemoryCard/>
-        </div>
-        <div className="grid-row">
-          <MemoryCard/>
-          <MemoryCard/>
-          <MemoryCard/>
-          <MemoryCard/>
-        </div>
-      
-      
       </div>
-      
-      
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
